8a7e4769605c7fc1e43a64ba8a0f3e624c31cf84
Dariusz Ciesielski <daron0407@gmail.com>
2022-10-07 13:33:26 +0200

    Rocketry Tune Up
    Copied Rocketry Tune Up from google drive

---
+++ Rocketry Tune Up/Rocketry Tune Up/Patches.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using TUNING;
+
+namespace Rocketry_Tune_Up
+{
+    [HarmonyPatch(typeof(KeroseneEngineClusterConfig), nameof(KeroseneEngineClusterConfig.DoPostConfigureComplete))]
+    public class PetroleumEngine
+    {
+        public static void Postfix(GameObject go)
+        {
+            RocketModulePerformance RMP = new RocketModulePerformance((float)ROCKETRY.BURDEN.MAJOR, ROCKETRY.FUEL_COST_PER_DISTANCE.HIGH, ROCKETRY.ENGINE_POWER.MID_VERY_STRONG);
+            go.GetComponent<RocketModuleCluster>().performanceStats = RMP;
+            go.GetComponent<Building>().Def.BuildingUnderConstruction.GetComponent<RocketModuleCluster>().performanceStats = RMP;
+        }
+    }
+    [HarmonyPatch(typeof(HydrogenEngineClusterConfig), nameof(HydrogenEngineClusterConfig.DoPostConfigureComplete))]
+    public class HydrogenEngine
+    {
+        public static void Postfix(GameObject go)
+        {
+            RocketModulePerformance RMP = new RocketModulePerformance((float)ROCKETRY.BURDEN.MAJOR_PLUS, ROCKETRY.FUEL_COST_PER_DISTANCE.MEDIUM, ROCKETRY.ENGINE_POWER.LATE_VERY_STRONG);
+            go.GetComponent<RocketModuleCluster>().performanceStats = RMP;
+            go.GetComponent<Building>().Def.BuildingUnderConstruction.GetComponent<RocketModuleCluster>().performanceStats = RMP;
+        }
+    }
+}
