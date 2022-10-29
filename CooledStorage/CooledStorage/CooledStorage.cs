6bf746e7056a4456e73af6d74680cb947431fb0b
Dariusz Ciesielski <daron0407@gmail.com>
2022-10-29 21:03:44 +0200

    Updated References
    Updated Refernces in Min Output Temp and Starmap Rebelanced

---
+++ CooledStorage/CooledStorage/CooledStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSerialization;
+using UnityEngine;
+
+namespace CooledStorage
+{
+    public class CooledLocker : StorageLocker
+    {
+
+    }
+}
