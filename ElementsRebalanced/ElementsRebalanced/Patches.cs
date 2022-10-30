e939a15ebcba3ab191d4f613ba3d3c87d226d172
Dariusz Ciesielski <daron0407@gmail.com>
2022-10-30 19:17:38 +0100

    Updated References
    Elements Rebalanced Reference Update & minor changes

--- ElementsRebalanced/ElementsRebalanced/Patches.cs
+++ ElementsRebalanced/ElementsRebalanced/Patches.cs
@@ -8,16 +8,20 @@ namespace ElementsRebalanced
         public class Db_Initialize_Patch
         {
             private static readonly float SuperCoolantLiquidGasTemp = 6000f;
-            private static readonly float ViscoGelHighTemp = 1300f;
-            private static readonly float ViscoGelLowTemp = 200f;
+            private static readonly float SuperCoolantThermalConductivity = 200f;
+
+            private static readonly float ViscoGelHighTemp = 1000f;
+            private static readonly float ViscoGelLowTemp = 100f;
             private static readonly float ViscoGelThermalConductivity = 0.001f;
             private static readonly float ViscoGelSpecificHeatCapacity = 6f;
-            private static readonly float ThermiumHighTemp = 3888f;
+
+            private static readonly float ThermiumHighTemp = 4000f;
             public static void Postfix(ElementLoader.ElementEntry entry, Element elem)
             {
                 if (entry.elementId.ToString().Equals("SuperCoolant"))
                 {
                     elem.highTemp = SuperCoolantLiquidGasTemp;
+                    elem.thermalConductivity = SuperCoolantThermalConductivity;
                 }
                 if (entry.elementId.ToString().Equals("SuperCoolantGas"))
                 {
