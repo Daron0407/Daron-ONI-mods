4d46aa48c2acf9efb168c48bcfc4a5db4a107130
Dariusz Ciesielski <daron0407@gmail.com>
2022-12-15 01:52:30 +0100

    ElementsRebalanced Decor and overheat expansion
    modified decor and overheat bonus for some materials

--- ElementsRebalanced/ElementsRebalanced/Patches.cs
+++ ElementsRebalanced/ElementsRebalanced/Patches.cs
@@ -1,4 +1,10 @@
 ﻿using HarmonyLib;
+using Klei.AI;
+using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
 
 namespace ElementsRebalanced
 {
@@ -7,37 +13,89 @@ namespace ElementsRebalanced
         [HarmonyPatch(typeof(ElementLoader), "CopyEntryToElement")]
         public class Db_Initialize_Patch
         {
-            private static readonly float SuperCoolantLiquidGasTemp = 6000f;
-            private static readonly float SuperCoolantThermalConductivity = 200f;
-
-            private static readonly float ViscoGelHighTemp = 1000f;
-            private static readonly float ViscoGelLowTemp = 100f;
-            private static readonly float ViscoGelThermalConductivity = 0.001f;
-            private static readonly float ViscoGelSpecificHeatCapacity = 6f;
-
-            private static readonly float ThermiumHighTemp = 4000f;
             public static void Postfix(ElementLoader.ElementEntry entry, Element elem)
             {
                 if (entry.elementId.ToString().Equals("SuperCoolant"))
                 {
-                    elem.highTemp = SuperCoolantLiquidGasTemp;
-                    elem.thermalConductivity = SuperCoolantThermalConductivity;
+                    elem.highTemp = 6000f;
+                    elem.thermalConductivity = 1000f;
                 }
                 if (entry.elementId.ToString().Equals("SuperCoolantGas"))
                 {
-                    elem.lowTemp = SuperCoolantLiquidGasTemp;
+                    elem.lowTemp = 6000f;
                 }
                 if (entry.elementId.ToString().Equals("ViscoGel"))
                 {
-                    elem.highTemp = ViscoGelHighTemp;
-                    elem.lowTemp = ViscoGelLowTemp;
-                    elem.thermalConductivity = ViscoGelThermalConductivity;
-                    elem.specificHeatCapacity = ViscoGelSpecificHeatCapacity;
+                    elem.highTemp = 1000f;
+                    elem.lowTemp = 100f;
+                    elem.thermalConductivity = 0.0001f;
+                    elem.specificHeatCapacity = 6f;
                 }
                 if (entry.elementId.ToString().Equals("TempConductorSolid"))
                 {
-                    elem.highTemp = ThermiumHighTemp;
+                    elem.highTemp = 4000f;
+                    elem.thermalConductivity = 300f;
                 }
+            }
+        }
+        [HarmonyPatch(typeof(LegacyModMain), "ConfigElements")]
+        public class LegacyModMainPatch
+        {
+            public static void modifyDecor(SimHashes hash, float amount)
+            {
+                Element elementByHash = ElementLoader.FindElementByHash(hash);
+                bool changed = false;
+                foreach(var v in elementByHash.attributeModifiers)
+                {
+                    if (v.AttributeId == "Decor")
+                    {
+                        v.SetValue(amount);
+                        changed = true;
+                    }
+                }
+                if (!changed)
+                {
+                    AttributeModifier atr = new AttributeModifier("Decor", amount, elementByHash.name, true);
+                    elementByHash.attributeModifiers.Add(atr);
+                }
+            }
+            public static void modifyOverheat(SimHashes hash, float amount)
+            {
+
+                Element elementByHash = ElementLoader.FindElementByHash(hash);
+
+                bool changed = false;
+                foreach (var v in elementByHash.attributeModifiers)
+                {
+                    if (v.AttributeId == Db.Get().BuildingAttributes.OverheatTemperature.Id)
+                    {
+                        v.SetValue(amount);
+                        changed = true;
+                    }
+                }
+                if (!changed)
+                {
+                    AttributeModifier atr = new AttributeModifier(Db.Get().BuildingAttributes.OverheatTemperature.Id, amount, elementByHash.name);
+                    elementByHash.attributeModifiers.Add(atr);
+                }
+            }
+            public static void Postfix()
+            {
+                modifyDecor(SimHashes.Cobalt, 0.5f);
+                
+                modifyDecor(SimHashes.Gold, 1.0f);
+
+                modifyDecor(SimHashes.Cobaltite, 0.1f);
+
+                modifyOverheat(SimHashes.Wolframite, 50f);
+                modifyOverheat(SimHashes.Tungsten, 200f);
+
+                modifyDecor(SimHashes.Ceramic, 1.0f);
+
+                modifyDecor(SimHashes.TempConductorSolid, 0.5f);
+                modifyOverheat(SimHashes.TempConductorSolid, 4000f);
+
+                modifyDecor(SimHashes.Glass, 1.0f);
             }
         }
     }
