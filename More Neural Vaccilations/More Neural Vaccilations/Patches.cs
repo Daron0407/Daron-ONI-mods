b77494082370f4adb67ff993b52d3ac1b17cbc99
Dariusz Ciesielski <daron0407@gmail.com>
2023-01-02 17:57:49 +0100

    More Vaccilations update

---
+++ More Neural Vaccilations/More Neural Vaccilations/Patches.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using TUNING;
+
+namespace ONI_mod
+{
+    public class Patches
+    {
+
+        [HarmonyPatch(typeof(Db))]
+        [HarmonyPatch("Initialize")]
+
+        public class DuplicantStatsPatch
+        {
+            public static void Postfix()
+            {
+                DUPLICANTSTATS.TraitVal[] traits = new DUPLICANTSTATS.TraitVal[]
+                {
+                    new DUPLICANTSTATS.TraitVal()
+                    {
+                        id = "RadiationEater",
+                        dlcId = "EXPANSION1_ID"
+                    },
+                    new DUPLICANTSTATS.TraitVal()
+                    {
+                        id = "StarryEyed",
+                        dlcId = "EXPANSION1_ID"
+                    }
+                };
+                foreach(var trait in traits)
+                {
+                    DUPLICANTSTATS.GENESHUFFLERTRAITS.Add(trait);
+                }
+
+            }
+
+        }
+    }
+}
