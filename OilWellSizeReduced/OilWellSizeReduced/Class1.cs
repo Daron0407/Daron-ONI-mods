928c18263639869b6c808679eaef7c750c35c481
Dariusz Ciesielski <daron0407@gmail.com>
2023-02-04 19:26:46 +0100

    OilWellTesting

---
+++ OilWellSizeReduced/OilWellSizeReduced/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace OilWellSizeReduced
+{
+    [HarmonyPatch(typeof(OilWellCapConfig), nameof(OilWellCapConfig.CreateBuildingDef))]
+    public class Patch
+    {
+        public static void Postfix(ref BuildingDef __result)
+        {
+            __result.WidthInCells = 1;
+            __result.HeightInCells = 1;
+        }
+    }
+}
