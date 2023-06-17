c57691cba4b4630ccb17bfc0965ffdca59f75347
Dariusz Ciesielski <daron0407@gmail.com>
2023-06-17 12:19:24 +0200

    Finished mod

--- sources/Renewable Abyssalite/Renewable Abyssalite/Starmap.cs
+++ sources/Renewable Abyssalite/Renewable Abyssalite/Starmap.cs
@@ -17,8 +17,9 @@ namespace Renewable_Abyssalite
         {
             foreach (HarvestablePOIConfig.HarvestablePOIParams harvestable in __result)
             {
-                if (harvestable.poiType.id.Equals("glimmering_asteroid_field"))
+                if (harvestable.poiType.id.Equals("GlimmeringAsteroidField"))
                 {
+                    Debug.Log("[Renewable_Abyssalite]: Starmap patch succesful");
                     harvestable.poiType.harvestableElements.Clear();
                     harvestable.poiType.harvestableElements[SimHashes.MoltenTungsten] = 2f;
                     harvestable.poiType.harvestableElements[SimHashes.Wolframite] = 6f;
@@ -44,10 +45,11 @@ namespace Renewable_Abyssalite
     {
         public static Harmony harmony_instance;
         public static bool patched = false;
-        public static void Postfix()
+        public static bool Prefix()
         {
             if (!patched)
             {
+                Debug.Log("[Renewable_Abyssalite]: Attempting to patch starmap");
                 patched = true;
                 var original = typeof(HarvestablePOIConfig).GetMethod("GenerateConfigs", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Static);
                 var prefix = typeof(StarmapPatch).GetMethod(nameof(StarmapPatch.dummy));
@@ -57,6 +59,11 @@ namespace Renewable_Abyssalite
                     harmony_instance.Patch(original, new HarmonyMethod(prefix), new HarmonyMethod(postfix));
                 }
             }
+            else
+            {
+                Debug.Log("[Renewable_Abyssalite]: Starmap patch failed");
+            }
+            return true;
         }
     }
 
