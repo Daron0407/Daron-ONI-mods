6efba9772be804f0ea2f34bc461fa00c14bae28f
Dariusz Ciesielski <daron0407@gmail.com>
2024-12-21 19:38:13 +0100

    Battery Bank fix

---
+++ sources/BionicFixes/BionicFixes/Patches.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BionicFixes
+{
+    [HarmonyPatch(typeof(ElectrobankConfig), nameof(ElectrobankConfig.CreatePrefab))]
+    public static class EcoPowerBank
+    {
+        public static void Postfix(ref GameObject __result)
+        {
+            __result.GetComponent<PrimaryElement>().MassPerUnit = 1f;
+        }
+    }
+
+    [HarmonyPatch(typeof(DisposableElectrobankConfig), "CreateDisposableElectrobank")]
+    public static class PowerBank
+    {
+        public static void Postfix(ref GameObject __result)
+        {
+            __result.GetComponent<PrimaryElement>().MassPerUnit = 1f;
+        }
+    }
+
+}
