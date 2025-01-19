534a84b9ad1379d21d78936f8a9c3b0ca7c517a0
Dariusz Ciesielski <daron0407@gmail.com>
2025-01-19 12:49:37 +0100

    Battery bank fix
    changed mass of depleted battery banks to 1 kg

--- sources/BionicFixes/BionicFixes/Patches.cs
+++ sources/BionicFixes/BionicFixes/Patches.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 namespace BionicFixes
 {
     [HarmonyPatch(typeof(ElectrobankConfig), nameof(ElectrobankConfig.CreatePrefab))]
-    public static class EcoPowerBank
+    public static class PowerBank
     {
         public static void Postfix(ref GameObject __result)
         {
@@ -18,7 +18,16 @@ namespace BionicFixes
     }
 
     [HarmonyPatch(typeof(DisposableElectrobankConfig), "CreateDisposableElectrobank")]
-    public static class PowerBank
+    public static class EcoPowerBank
+    {
+        public static void Postfix(ref GameObject __result)
+        {
+            __result.GetComponent<PrimaryElement>().MassPerUnit = 1f;
+        }
+    }
+
+    [HarmonyPatch(typeof(EmptyElectrobankConfig), nameof(EmptyElectrobankConfig.CreatePrefab))]
+    public static class EmptyPowerBank
     {
         public static void Postfix(ref GameObject __result)
         {
