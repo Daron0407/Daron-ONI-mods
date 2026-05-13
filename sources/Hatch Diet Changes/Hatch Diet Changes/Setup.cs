c1e7fef8145107ec8e9f7e3ede86e56e00ad59eb
Dariusz Ciesielski <daron0407@gmail.com>
2026-05-13 23:48:28 +0200

    Hatch diet changes version update

---
+++ sources/Hatch Diet Changes/Hatch Diet Changes/Setup.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+public class Setup
+{
+    // Already existing
+    public static HashSet<Tag> regulaHatchCarbon()
+    {
+        return new HashSet<Tag>() { SimHashes.Regolith.CreateTag(), SimHashes.Salt.CreateTag() };
+    }
+
+    public static List<Tuple<Tag, Tag>> regularHatchMetals()
+    {
+        List<Tuple<Tag, Tag>> result = new List<Tuple<Tag, Tag>>() {
+            new Tuple<Tag, Tag>(SimHashes.Iron.CreateTag(), SimHashes.IronOre.CreateTag()),
+            new Tuple<Tag, Tag>(SimHashes.Copper.CreateTag(), SimHashes.Cuprite.CreateTag()),
+            new Tuple<Tag, Tag>(SimHashes.Gold.CreateTag(), SimHashes.GoldAmalgam.CreateTag()),
+            new Tuple<Tag, Tag>(SimHashes.Aluminum.CreateTag(), SimHashes.AluminumOre.CreateTag()),
+            new Tuple<Tag, Tag>(SimHashes.Tungsten.CreateTag(), SimHashes.Wolframite.CreateTag()),
+        };
+
+        if (DlcManager.IsExpansion1Active())
+        {
+            result.Add(new Tuple<Tag, Tag>(SimHashes.Cobalt.CreateTag(), SimHashes.Cobaltite.CreateTag()));
+            result.Add(new Tuple<Tag, Tag>(SimHashes.DepletedUranium.CreateTag(), SimHashes.UraniumOre.CreateTag()));
+        }
+        if (Game.IsDlcActiveForCurrentSave(DlcManager.DLC2_ID))
+        {
+            result.Add(new Tuple<Tag, Tag>(SimHashes.SolidMercury.CreateTag(), SimHashes.Cinnabar.CreateTag()));
+        }
+        if (Game.IsDlcActiveForCurrentSave(DlcManager.DLC3_ID))
+        {
+            result.Add(new Tuple<Tag, Tag>(SimHashes.Nickel.CreateTag(), SimHashes.NickelOre.CreateTag()));
+        }
+
+        return result;
+    }
+
+    public static HashSet<Tag> veggieHatchCarbon()
+    {
+        var result = new HashSet<Tag>()
+        {
+            SimHashes.Sulfur.CreateTag(),
+            SimHashes.Phosphorite.CreateTag(),
+            SimHashes.Salt.CreateTag()
+        };
+        if (DlcManager.IsExpansion1Active())
+        {
+            result.Add(SimHashes.Mud.CreateTag());
+            result.Add(SimHashes.ToxicMud.CreateTag());
+        }
+        if (Game.IsDlcActiveForCurrentSave(DlcManager.DLC3_ID))
+        {
+            result.Add(SimHashes.Peat.CreateTag());
+        }
+        return result;
+    }
+
+
+    public static List<Tuple<Tag, Tag>> stoneHatchHalf()
+    {
+        return new List<Tuple<Tag, Tag>>()
+        {
+            new Tuple<Tag, Tag>(SimHashes.Katairite.CreateTag(), SimHashes.Wolframite.CreateTag()),
+            new Tuple<Tag, Tag>(SimHashes.Glass.CreateTag(), SimHashes.Obsidian.CreateTag()),
+        };
+    }
+
+    public static Tuple<Tag, Tag> stoneHatchQuarter()
+    {
+        return new Tuple<Tag, Tag>(SimHashes.Fossil.CreateTag(), SimHashes.Lime.CreateTag());
+    }
+
+    // Smooth hatch extras
+    public static List<Tuple<Tag, Tag>> smoothHatchExtras()
+    {
+        return new List<Tuple<Tag, Tag>>()
+        {
+            new Tuple<Tag, Tag>(SimHashes.RefinedCarbon.CreateTag(), SimHashes.Diamond.CreateTag()),
+            new Tuple<Tag, Tag>(SimHashes.Rust.CreateTag(), SimHashes.Iron.CreateTag())
+        };
+    }
+}
